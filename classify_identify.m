c4af738f6a778c431abf358c33457a9929d4145b
Sanzida Luna <sanzidaluna07@gmail.com>
2022-11-28 14:33:12 -0500

    KNN identify

--- classify_identify.m
+++ classify_identify.m
@@ -1,4 +1,4 @@
-function [class_01,class_02] = classify_identify(type_c,type_i)
+function [class_01,class_02] = classify_identify(type)
 
 
     train_data = load_database(1);
@@ -11,19 +11,48 @@ function [class_01,class_02] = classify_identify(type_c,type_i)
     sub_data = test_data - repmat(mean_features,1,n_test); 
     updated_test_data = principle_basis'*sub_data;
 
-    if type_c == 1 %KMeans classification
+    if type == 1 
+        
+        %KMeans classification
 
         class_01 = kmeans (updated_test_data', 2);
-
-    elseif type_i == 1 %KMeans identification
         
-        updated_test_data (:,121:130) = [];
+        %KMeans identification
+
+        updated_test_data (:,121:m_test) = [];
         class_02 = kmeans (updated_test_data', 40);
 
-    else
+    elseif type == 2 
+        
+        %KNN classification
 
-                       
+        [idx, D] = knnsearch (Y', updated_test_data');
+        mean_D = mean(D);
+        for i = 1:n_test
+            if D(i) < mean_D
+                class_01(i) = 1;
+            else
+                class_01(i) = 2;
+            end
+        end
+        ideal_class01(1:120) = 1;
+        ideal_class01(121:n_test) = 2;
+        sum = 0;
+        for i = 1:n_test
+            if ideal_class01(i) == class_01(i)
+                sum = sum + 1;
+            end
+        end
+        confidence = sum / n_test;
+        disp(confidence);
+
+        %KNN identification
+
+        updated_test_data (:,121:n_test) = [];
+        [idx, D] = knnsearch (Y', updated_test_data');
+        disp(idx');
     end
+                       
         
 end
 
--- main.m
+++ main.m
@@ -2,13 +2,14 @@
 %eigenface()
 %disp('Reconstructing from Eigenfaces')
 %reconstruct()
-disp('Classification and identification with KMeans')
-[class_01,class_02] = classify_identify(1);
-[m,n] = size(class_01);
-x = 1:m;
-y = class_01;
-plot(y,x,'*')
+%disp('Classification and identification with KMeans')
+%[class_01,class_02] = classify_identify(1,1);
+%[m,n] = size(class_01);
+%x = 1:m;
+%y = class_01;
+%plot(y,x,'*')
 %xlim([0 3])
 %ylim([0 140])
-disp(class_02');
-	
+%disp(class_02');
+disp('Classification and identification with KNN')
+[class_01,class_02] = classify_identify(2);	
